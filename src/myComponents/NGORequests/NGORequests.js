717cbfe3556724edf5a013e8b7730755492136cc
Rija Nadeem <rijanadeem42@gmail.com>
2020-05-22 01:55:45 +0500

    ngo request page
    almost done

--- src/myComponents/NGORequests/NGORequests.js
+++ src/myComponents/NGORequests/NGORequests.js
@@ -32,10 +32,10 @@ class NGORequests extends React.Component {
                 {backdrop}
                 <div className="ngo-main-div">
                     <div className="filter-area">
+                    <h2 className="filter-heading">Filter Requests:</h2>
                         <ul className="filter-options">
-                            <li><h2 className="filter-heading">Filter Requests:</h2></li>
                             <li><a href="/">All</a></li>
-                            <li><a href="/">Human Rights Organization</a></li>
+                            <li><a href="/">Human Rights Organization </a></li>
                             <li><a href="/">Helping Hands</a></li>
                             <li><a href="/">Justice For You</a></li>
                             <li><a href="/">Little Care</a></li>
@@ -45,10 +45,19 @@ class NGORequests extends React.Component {
                         </ul>
                     </div>
                     <div class="request-area">
-                        <h1 className="request-area-heading">NGO'S REQUESTS</h1>
+                        <h2 className="request-area-heading">NGO'S REQUESTS</h2>
                         
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                            Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class="text-right"><a href="/" class="btn btn-primary " style={{fontWeight:"bold"}}>Donate NOW</a></div>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
                             Some quick example text to build on the card title and make up the bulk of the card's content.
                             Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -56,6 +65,15 @@ class NGORequests extends React.Component {
                         </div>
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                            Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class="text-right"><a href="/" class="btn btn-primary " style={{fontWeight:"bold"}}>Donate NOW</a></div>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
                             Some quick example text to build on the card title and make up the bulk of the card's content.
                             Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -63,25 +81,23 @@ class NGORequests extends React.Component {
                         </div>
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
                             Some quick example text to build on the card title and make up the bulk of the card's content.
                             Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <div class="text-right"><a href="#" class="btn btn-primary " style={{fontWeight:"bold"}}>Donate NOW</a></div>
+                            <div class="text-right"><a href="/" class="btn btn-primary " style={{fontWeight:"bold"}}>Donate NOW</a></div>
                         </div>
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
                             Some quick example text to build on the card title and make up the bulk of the card's content.
                             Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <div class="text-right"><a href="#" class="btn btn-primary " style={{fontWeight:"bold"}}>Donate NOW</a></div>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                            Some quick example text to build on the card title and make up the bulk of the card's content.
-                            Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <div class="text-right"><a href="#" class="btn btn-primary " style={{fontWeight:"bold"}}>Donate NOW</a></div>
-                        </div>
+
+
+                        
                     </div>
                 </div>
                 <Footer/>
